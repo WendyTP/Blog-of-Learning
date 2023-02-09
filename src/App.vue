cde6a00abb89deeac501d12d8d19ced1794df806
Wendy <wendy.tsai1988@gmail.com>
2023-02-09 19:14:09 +0100

    add header and footer vue templates

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import BlogHeader from './components/BlogHeader.vue'
+import BlogFooter from './components/BlogFooter.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+ 
+  <BlogHeader />
 
   <main>
     <TheWelcome />
   </main>
+
+  <BlogFooter />
 </template>
 
 <style scoped>
