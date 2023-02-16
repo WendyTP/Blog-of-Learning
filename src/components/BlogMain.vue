ffc77226351a1d7145651e19d426f10d57353adc
Wendy <wendy.tsai1988@gmail.com>
2023-02-16 15:24:37 +0100

    import json data and update BlogMain and BlogMultiplePosts vue with imported json data

---
+++ src/components/BlogMain.vue
@@ -0,0 +1,36 @@
+<script setup>
+  import BlogFeaturedPost from '../components/BlogFeaturedPost.vue'
+  import BlogMultiplePosts from '../components/BlogMultiplePosts.vue'
+  import BlogNavBar from '../components/BlogNavBar.vue'
+  import json from '../assets/site_structure.json'
+ 
+  import {ref, onMounted} from 'vue'
+
+  const count = ref(0)
+  const allPosts = ref(json.posts)
+
+  
+  
+</script>
+
+<template>
+  <BlogFeaturedPost />
+ 
+
+  <div class="container">
+    <div class="row g-5 mb-4">
+      <BlogMultiplePosts :postData="allPosts" />
+      
+      <div class="navBar col-md-4 ">
+        <BlogNavBar />
+      </div>
+    </div>
+  </div>
+
+  
+
+  
+  
+
+</template>
+<style scoped></style>
---
+++ src/components/BlogMultiplePosts.vue
@@ -0,0 +1,44 @@
+<script setup>
+  defineProps({
+    postData: {
+      type: Array,
+      required: true
+    }
+})
+</script>
+<template>
+  <div class="postsContainer col-md-8">
+    <article class="blog-post">
+      <h2 class="blog-post-title mb-1">Another blog post</h2>
+      <p class="blog-post-meta">December 23, 2020 by <a href="#">Jacob</a></p>
+      <img src="https://via.placeholder.com/800x300.jpg" alt="image of first post" class="img-fluid">
+
+      <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
+      <blockquote>
+        <p>Longer quote goes here, maybe with some <strong>emphasized text</strong> in the middle of it.</p>
+      </blockquote>
+      <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
+      <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout.</p>
+    </article>
+
+    <article class="blog-post" v-for="post in postData" :key="post.id">
+      <h2 class="blog-post-title mb-1">{{post.title}}</h2>
+      <p class="blog-post-meta">{{post.publishedDate}} by <a href="#">{{post.author}}</a></p>
+      <img src="{{post.titleImage}}" alt="image of first post" class="img-fluid">
+
+      <p>{{post.content}}</p>
+    </article>
+
+    
+
+    
+
+    <nav class="blog-pagination" aria-label="Pagination">
+      <a class="btn btn-outline-primary rounded-pill" href="#">Older</a>
+      <a class="btn btn-outline-secondary rounded-pill disabled">Newer</a>
+    </nav>
+
+  </div>
+
+</template>
+<style scoped></style>
